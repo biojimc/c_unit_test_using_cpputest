008912601426d080446ed83bf7174fb306b32f72
biojimc <biojimc@gmail.com>
2025-06-17 07:34:11 +0000

    delete old files

--- src/get_sensor_value.c
+++
@@ -1,3 +0,0 @@
-int get_sensor_value(void) {
-    return 123;
-}
--- src/sensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef SENSOR_H
-#define SENSOR_H
-
-int get_sensor_value(void);  // 會被 mock
-int process_sensor(void);    // 被測試函式
-
-#endif // SENSOR_H
